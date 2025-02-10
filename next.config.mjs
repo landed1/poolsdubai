a1714e910bfb71fe97d9e99dcd7e7643410e692e
Calvin Crane <1049799+landed1@users.noreply.github.com>
2025-02-10 10:30:41 +0000

    redirect fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,6 @@ const nextConfig = {
   basePath: isProd ? "" : "",
   output: "export",
   images: { unoptimized: true },
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: "/pool-chemicals-supply-dubai/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
