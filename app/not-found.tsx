18f934ae307bbee31e42ed4df5fd79b094f2ee2d
Calvin Crane <1049799+landed1@users.noreply.github.com>
2025-02-10 16:45:54 +0000

    Improve not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,24 @@
-export default function NotFound(){
-return(
-    <div>
-        <h2>Not Found</h2>
+import NextBreadcrumb from "./components/NextBreadcrumb";
+
+export default function NotFound() {
+  return (
+    <div className='my-20'>
+      <NextBreadcrumb
+        homeElement={"Home"}
+        separator={<span> &gt; </span>}
+        activeClasses='text-amber-500'
+        containerClasses='flex py-5'
+        listClasses='hover:underline mx-2 font-bold'
+        capitalizeLinks
+      />
+      <article className='flex-grow page-gutter'>
+        <h1 className='text-4xl font-bold text-center text-[#0fa0ba] mb-8'>
+          You found the secret page!
+        </h1>
+        <div>
+          <h2>Page Not Found</h2>
+        </div>
+      </article>
     </div>
-)
+  );
 }
