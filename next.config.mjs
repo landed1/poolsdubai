8e3f9cb1b302642e032dd674a964bee462f8277b
Calvin Crane <1049799+landed1@users.noreply.github.com>
2025-02-10 15:04:13 +0000

    testing basePath change

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "" : "",
+  basePath: isProd ? "poolsdubai" : "",
   output: "export",
   images: { unoptimized: true },
 };
